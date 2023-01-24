01355f7df58098ba5adb1a204fac551e63a7b698
Intrus1ve <98790607+Intrus1ve@users.noreply.github.com>
2023-01-25 00:50:32 +0300

    Add files via upload

---
+++ Telegram.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import requests
+import sys
+
+def telegram_bot_msgSender(msg):
+    teleBot_token = "5850397054:AAHdM-f6X9ClO2KCSUxtCWErzvCE9pcx1ao"
+    teleBot_chatID = "1074075245"
+    send_msg = (
+        "https://api.telegram.org/bot"
+        + teleBot_token
+        + "/sendMessage?chat_id="
+        + teleBot_chatID
+        + "&parse_mode=Markdown&text="
+        + msg
+    )
+    response = requests.get(send_msg)
+    return response.json()
+
+telegram_bot_msgSender(sys.argv[1])
